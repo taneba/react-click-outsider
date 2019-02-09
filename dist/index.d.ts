2abea76776ce1e072ed13e434fedbb62f65aa12d
ynakamura <ynakamura@r.recruit.co.jp>
2019-02-10 04:32:22 +0900

    ignore dist

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-interface Props {
-    onClickOutside: (e: MouseEvent | TouchEvent) => void;
-    children: React.ReactNode;
-}
-export default function ClickOutside(props: Props): JSX.Element;
-export {};
